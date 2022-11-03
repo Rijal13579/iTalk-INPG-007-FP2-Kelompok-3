850ec82d122c92fdb64fc366767b75d26fa50287
yoohhhaaaannnnnaaaaaa <yohanavanesa7@gmail.com>
2022-11-03 17:14:45 +0700

    membuat listener untuk password

--- js/signUp.js
+++ js/signUp.js
@@ -1,8 +1,10 @@
 let formatSpecial = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 let formatEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let formatCondition = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/
 
 let usernameSignUpListener = document.getElementById('signUpUsername');
 let emailSignUpListener = document.getElementById('signUpEmail');
+let password1SignUpListener = document.getElementById("signUpPassword1")
 
 usernameSignUpListener.addEventListener('keyup', () => {
   let usernameSigUp = usernameSignUpListener.value;
@@ -34,6 +36,29 @@ emailSignUpListener.onkeyup = () => {
     document.getElementById('emailFeedback').innerHTML = '';
   }
 };
+
+password1SignUpListener.onkeyup = () => {
+  let password1SignUp = password1SignUpListener.value
+  let validateLength = password1SignUp.length >= 6 && password1SignUp.length <= 12
+  let validateCondition = formatCondition.test(password1SignUp)
+  let validateNoSpecial = !formatSpecial.test(password1SignUp)
+
+  let message = '';
+  message += validateLength ? '' : 'Password kurang dari 6 atau lebih dari 12 karakter'
+  message += validateLength || validateCondition ? '' : '<br>'
+  message += validateCondition ? '' : 'Password minimal harus mengandung 1 huruf kecil, 1 huruf besar, dan 1 angka'
+  message += validateCondition || validateNoSpecial ? '' : '<br>'
+  message += validateNoSpecial ? '' : 'Password tidak boleh mengandung spasi atau karakter spesial'
+
+  console.log(message);
+  if (message) {
+    password1SignUpListener.setCustomValidity(message)
+    document.getElementById("password1Feedback").innerHTML = message
+  } else {
+    password1SignUpListener.setCustomValidity('')
+    document.getElementById("password1Feedback").innerHTML = ''
+  }
+}
 
 (function () {
   'use strict';
