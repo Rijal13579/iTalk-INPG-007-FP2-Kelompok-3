44b3c956c74dbd3f4f553aae12315c2a48da2487
yoohhhaaaannnnnaaaaaa <yohanavanesa7@gmail.com>
2022-11-03 17:41:43 +0700

    membuat pengecekan login dan timer login

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+let loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
+if (loggedIn) {
+  let oneMin = 60 * 1000;
+  let loggedInDisplay = document.getElementById("loggedIn")
+  loggedInDisplay.getElementsByTagName('button')[0].innerHTML = loggedIn.username
+  document.getElementById("loggedOut").style.display = 'none'
+  loggedInDisplay.style.display = 'block'
+  setTimeout(() => {
+    localStorage.removeItem('loggedIn')
+    window.location.replace(window.location.href)
+  }, oneMin*3);
+}
 // navonscroll
 hide_on_scroll({
     nav_id : 'demo1Navbar',
