69e3016d80f1374be4cd2e664eead57c6b17deac
yoohhhaaaannnnnaaaaaa <yohanavanesa7@gmail.com>
2022-11-03 20:46:08 +0700

    function log out saat tab di tutup

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
+var logout = () => {};
 let loggedIn = JSON.parse(localStorage.getItem('loggedIn'));
 if (loggedIn) {
+  logout = () => {
+    localStorage.removeItem('loggedIn');
+  };
   let oneMin = 60 * 1000;
-  let loggedInDisplay = document.getElementById("loggedIn")
-  loggedInDisplay.getElementsByTagName('button')[0].innerHTML = loggedIn.username
-  document.getElementById("loggedOut").style.display = 'none'
-  loggedInDisplay.style.display = 'block'
+  let loggedInDisplay = document.getElementById('loggedIn');
+  loggedInDisplay.getElementsByTagName('button')[0].innerHTML = loggedIn.username;
+  document.getElementById('loggedOut').style.display = 'none';
+  loggedInDisplay.style.display = 'block';
   setTimeout(() => {
-    localStorage.removeItem('loggedIn')
-    window.location.replace(window.location.href)
-  }, oneMin*3);
+    localStorage.removeItem('loggedIn');
+    window.location.replace(window.location.href);
+  }, oneMin * 3);
 }
 // navonscroll
 hide_on_scroll({
-    nav_id : 'demo1Navbar',
-    hide_onscroll_mobile : true,
-    nav_offset : 256,
-  });
+  nav_id: 'demo1Navbar',
+  hide_onscroll_mobile: true,
+  nav_offset: 256,
+});
